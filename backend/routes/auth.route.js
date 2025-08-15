17eb1998ad8543c2119f0f1415749b4a72933ec4
Aditya Pradhan <144414442+adityapradhan123@users.noreply.github.com>
2025-08-15 18:28:08 +0530

    Implemented email OTP verification flow

---
+++ backend/routes/auth.route.js
@@ -0,0 +1,11 @@
+import express from "express";
+import { signup, verifyOTP, login } from "../controllers/auth.controller.js";
+
+const router = express.Router();
+
+router.post("/signup", signup);
+router.post("/verify-otp", verifyOTP);
+router.post("/login", login);
+
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
 import testRoutes from "./routes/test.route.js";
+import connectDB from './config/db.js';
+import authRoutes from "./routes/auth.route.js";
+
 
 
 // Load environment variables
@@ -14,14 +17,18 @@ app.use(express.json());
 
 // Routes
 app.use("/api/test", testRoutes);
+app.use("/api/auth",authRoutes)
 
 // Test route
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
+//Database Connection
+connectDB();
 // Server listen
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
